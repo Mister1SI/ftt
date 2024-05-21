d3302a46a6afb5ad806dfb99f523c38160d38106
Mister1SI <wazzup2690@gmail.com>
2024-05-20 21:07:37 -0700

    Add help menu, optimize, don't dereference bad memory

--- src/main.zig
+++ src/main.zig
@@ -6,25 +6,28 @@ pub fn main() !void {
     var args_iter = try std.process.argsWithAllocator(allocator);
     defer args_iter.deinit();
 
-    const stdout = std.io.getStdOut();
-    defer stdout.close();
-    const writer = stdout.writer();
+    const writer = std.io.getStdOut().writer();
 
     _ = args_iter.next();
 
     while (args_iter.next()) |arg| {
         if (arg[0] == '-') {
-            if (arg[1] == '-') {
+            if (arg.len > 1 and arg[1] == '-') {
                 // Option
+                const port: []const u8 = "--port=";
+                if (std.mem.startsWith(u8, arg, port)) {
+                    const port_num = arg[7..];
+                    try writer.print("Port: {s}", .{port_num});
+                }
             } else {
                 // Flag
                 for (arg, 0..) |c, i| {
                     if (i == 0) continue;
                     switch (c) {
                         'h' => try help(),
-                        'c' => try stdout.writeAll("Launching in client mode.\n"),
-                        's' => try stdout.writeAll("Launching in server mode.\n"),
-                        else => try writer.print("Unrecognized flag {c}.\n", .{c}),
+                        's' => try writer.writeAll("Launching in sender mode.\n"),
+                        'r' => try writer.writeAll("Launching in reciever mode.\n"),
+                        else => try writer.print("Unrecognized flag: {c}.\n", .{c}),
                     }
                 }
             }
@@ -36,10 +39,21 @@ pub fn main() !void {
 
 fn help() !void {
     const help_text =
-        \\This is really useful help text.
-        \\It's helping you.
-        \\You are being helped.
-        \\Do not question.
+        \\ftt
+        \\File Transfer Tool
+        \\usage:
+        \\    ftt -s [-flags] [--options] <files>
+        \\    ftt -r 
+        \\
+        \\options:
+        \\    --port=   Set the port.
+        \\    --ip=     Set the IPv4 address.
+        \\
+        \\flags:
+        \\    -h        Open the help menu.
+        \\    -s        Start in sender mode.
+        \\    -r        Start in reciever mode.
+        \\
         \\
     ;
     try std.io.getStdOut().writeAll(help_text);
