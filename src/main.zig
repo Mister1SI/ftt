cd479818176671b5b3a12e63f38b217e08a04920
Mister1SI <wazzup2690@gmail.com>
2024-05-20 19:05:41 -0700

    Add command line argument processing

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,37 @@
 const std = @import("std");
+const dbgPrint = std.debug.print;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const allocator = std.heap.page_allocator;
+    var args_iter = try std.process.argsWithAllocator(allocator);
+    defer args_iter.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const stdout = std.io.getStdOut();
+    defer stdout.close();
+    // const writer = stdout.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    _ = args_iter.next();
 
-    try bw.flush(); // don't forget to flush!
+    while (args_iter.next()) |arg| {
+        if (arg[0] == '-') {
+            if (arg[1] == '-') {
+                // Option
+            } else {
+                // Flag
+            }
+        } else {
+            // File
+        }
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn help() !void {
+    const help_text =
+        \\This is really useful help text.
+        \\It's helping you.
+        \\You are being helped.
+        \\Do not question.
+        \\
+    ;
+    try std.io.getStdOut().writeAll(help_text);
 }
